801258e446e4459b4604c4b616e80941b0a338a3
William <wnazate521@gmail.com>
2022-08-11 19:34:29 -0500

    tutorial v2

--- tuitorial.py
+++ tuitorial.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
-list_input = st.text_input("List")
-st.write(f"You enter a {list_input}")
+
+
+st.header("Remove duplicate list items")
+st.caption("Do you have duplicate items in your list which you want to remove? You can do that with only one line of code using the set() function.")
+list_input = st.text_input("Enter a List, example: 1,1,2,3,4,4....")
+list1 = list(list_input)
+new_list = set(list1)
+st.subheader("Output")
+st.write(new_list)
